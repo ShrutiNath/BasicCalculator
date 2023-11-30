ffdd35290fa8ce856024ca239e666f58a16940a8
Shruti Nath <shruti280502@gmail.com>
2023-12-01 00:34:14 +0530

    documented

--- script.js
+++ script.js
@@ -2,26 +2,24 @@ let result = document.getElementById('result');
 let nums = document.querySelectorAll('li');
 
 for(let i=0; i<nums.length; i++){
-    //console.log(nums[i].innerHTML);
 
     result.value.innerHTML;
     nums[i].addEventListener('click',function(){
-        //console.log(nums[i].innerHTML);
-        let getvalue = document.getElementById('result').value;
+        let getvalue = document.getElementById('result').value; 
 
         if(nums[i].innerHTML=="="){
-            result.value = eval(result.value)
+            result.value = eval(result.value)     // Displays result
         }
         else{
             if(nums[i].innerHTML == "C" || nums[i].innerHTML == "CE"){
-                result.value = "";
+                result.value = "";      // clears the screen
             }
             else{
                 result.value += nums[i].innerHTML;
             }
 
             if(nums[i].innerHTML=="Del"){
-                result.value=getvalue.slice(0, -1);
+                result.value=getvalue.slice(0, -1);  // deletes the previous character
             }
         }
     })
